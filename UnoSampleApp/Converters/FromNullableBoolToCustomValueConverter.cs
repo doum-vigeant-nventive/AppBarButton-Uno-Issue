17a6cfebb720e3d2a4f6d1c39197a11148466421
Dominique Vigeant <dominique.vigeant@nventive.com>
2023-04-28 13:45:08 -0400

    Initial commit

---
+++ UnoSampleApp/Converters/FromNullableBoolToCustomValueConverter.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Globalization;
+
+namespace UnoSampleApp.Converters;
+
+public class FromNullableBoolToCustomValueConverter : IValueConverter
+{
+    public object NullOrFalseValue { get; set; }
+
+    public object TrueValue { get; set; }
+
+    object IValueConverter.Convert(object value, Type targetType, object parameter, string culture)
+    {
+        return Convert(value, targetType, parameter);
+    }
+
+    object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string culture)
+    {
+        return ConvertBack(value, targetType, parameter);
+    }
+
+    public object Convert(object value, Type targetType, object parameter)
+    {
+        if (parameter != null)
+        {
+            throw new ArgumentException($"This converter does not use any parameters. You should remove \"{parameter}\" passed as parameter.");
+        }
+
+        if (value != null && !(value is bool))
+        {
+            throw new ArgumentException($"Value must either be null or of type bool. Got {value} ({value.GetType().FullName})");
+        }
+
+        if (value == null)
+        {
+            return NullOrFalseValue;
+        }
+        else
+        {
+            return TrueValue;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter)
+    {
+        if (parameter != null)
+        {
+            throw new ArgumentException($"This converter does not use any parameters. You should remove \"{parameter}\" passed as parameter.");
+        }
+
+        if (object.Equals(this.TrueValue, this.NullOrFalseValue))
+        {
+            throw new InvalidOperationException("Cannot convert back if both custom values are the same");
+        }
+
+        return this.TrueValue != null ?
+            value.Equals(TrueValue) :
+            !value.Equals(this.NullOrFalseValue);
+    }
+}
---
+++ UnoSampleApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoSampleApp;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public partial class MainPage : Page, INotifyPropertyChanged
+{
+    private bool _isOrdering;
+
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+
+    void ToggleReorder(object sender, RoutedEventArgs e)
+    {
+        IsOrdering = !IsOrdering;
+    }
+
+    public bool IsOrdering
+    {
+        get => _isOrdering;
+        set
+        {
+            _isOrdering = value;
+            OnPropertyChanged(nameof(IsOrdering));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
